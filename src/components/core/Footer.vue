cdcac57e1e2cfc93fb5e60716a064f9dfca21d77
Mohammad Azhar <mohammad.azhar@amadeus.com>
2019-10-28 14:46:41 +0800

    Simple Pwa Weather App

---
+++ src/components/core/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-footer
+    id="core-footer"
+    absolute
+    height="82"
+  >
+    <div class="footer-items">
+      <span
+        v-for="link in links"
+        :key="link.name"
+      >
+        <a
+          :href="link.Link"
+          class="tertiary--text footer-links">{{ link.name }}</a>
+      </span>
+    </div>
+    <v-spacer/>
+    <span class="font-weight-light copyright">
+      &copy;
+      {{ (new Date()).getFullYear() }}
+      <a
+        href="https://www.linkedin.com/in/azhar-mohammad-a6208474/"
+        target="_blank">Mohammad Azhar</a>, made with
+      <v-icon
+        color="tertiary"
+        size="17">mdi-heart</v-icon>
+      for a better web
+    </span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { name: 'Home', Link: '/dashboard' },
+      { name: 'Mohammad Azhar', Link: 'https://www.linkedin.com/in/azhar-mohammad-a6208474/' },
+      { name: 'Github', Link: 'https://github.com/azharzzz' }
+    ]
+  })
+}
+</script>
+
+<style>
+#core-footer {
+  z-index: 0;
+}
+</style>
